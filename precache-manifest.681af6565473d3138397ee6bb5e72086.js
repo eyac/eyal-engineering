8ddb8b827d8b0891dc8eefd77c46d7877899a74e
ecohen2 <eyal_cohen@intuit.com>
2020-01-12 21:20:58 +0200

    Updates

---
+++ precache-manifest.681af6565473d3138397ee6bb5e72086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e48e8c8cfd2c19492c90d0a9b622bb0",
+    "url": "/maya/index.html"
+  },
+  {
+    "revision": "70d24edc0ab8ee067b09",
+    "url": "/maya/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e014d316557e7bb173f7",
+    "url": "/maya/static/js/2.0a79c142.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/maya/static/js/2.0a79c142.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70d24edc0ab8ee067b09",
+    "url": "/maya/static/js/main.2d781b0d.chunk.js"
+  },
+  {
+    "revision": "724d90f3553f8c711f74",
+    "url": "/maya/static/js/runtime-main.01a4e7a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maya/static/media/logo.5d5d9eef.svg"
+  }
+]);
